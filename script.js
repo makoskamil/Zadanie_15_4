e8976863138f517ed8e9cd497b44e563f8291d1d
Kamil Makoś <makos.kamil@o2.pl>
2018-06-13 08:04:39 +0200

    add_task_5

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//zadanie 1
+const addedStrings = (string1='Hello', string2='world') => console.log(`${string1} ${string2}`);
+addedStrings();
+
+//zadanie 2
 const multiply = (number1 = 1, number2 = 1) => number1 * number2;
 console.log(multiply(2));
 console.log(multiply(6,4));
+
+//zadanie 3
+const average = (...args) => {
+    let sum = 0;
+    args.forEach(arg => {sum += arg});
+return (`Srednia liczb wynosi: ${sum/args.length}`);
+}
+
+//zadanie4
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+average(...grades);
+
+//zadanie5
+const array = [1, 4, 'Iwona', false, 'Nowak'];
+
+const [, , firstName, ,lastName]=array;
+
+console.log(`The frist name is: ${firstName}`);
+console.log(`The last name is: ${lastName}`);
